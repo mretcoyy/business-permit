011676dda163251949546d7c4bb3126ec511cae1
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2023-06-29 23:40:19 +0800

    feat: install ant design

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,13 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+
+import "ant-design-vue/dist/antd.css";
+import Antd from "ant-design-vue";
+Vue.use(Antd);
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +23,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +35,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
 });
