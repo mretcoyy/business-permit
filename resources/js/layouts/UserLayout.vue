7eb58d385de5043a365fb3d5f3e4d4ef0fb6b3a0
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2023-07-10 20:57:27 +0800

    feat: fix errors

--- resources/js/layouts/UserLayout.vue
+++ resources/js/layouts/UserLayout.vue
@@ -1,24 +1,18 @@
 <template>
     <div id="userLayout" :class="['user-layout-wrapper', isMobile && 'mobile']">
         <div class="container">
-            <router-view />
             <slot></slot>
-            <!-- <div class="footer">
-          <div class="links">
-            <a href="_self">帮助</a>
-            <a href="_self">隐私</a>
-            <a href="_self">条款</a>
-          </div>
-          <div class="copyright">
-            Copyright &copy; 2018 vueComponent
-          </div>
-        </div> -->
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            isMobile: false,
+        };
+    },
     name: "UserLayout",
     mounted() {
         document.body.classList.add("userLayout");
