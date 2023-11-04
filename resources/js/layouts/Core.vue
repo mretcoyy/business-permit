c144dc3cee063589878ba2938d49a185fbc0043f
devitcoy <mretcoyy@gmail.com>
2023-11-04 23:51:48 +0800

    User Setup

---
+++ resources/js/layouts/Core.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>asd</div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: String,
+        }
+    },
+    mounted() {
+        console.log(this.user);
+    },
+    method:{
+    
+    },
+};
+</script>
+
