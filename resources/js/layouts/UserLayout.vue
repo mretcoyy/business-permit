42de26e6b4d8319f522ffe2159526bcc77d17166
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2023-11-04 23:27:05 +0800

    feat: ui user layout

--- resources/js/layouts/UserLayout.vue
+++ resources/js/layouts/UserLayout.vue
@@ -1,53 +1,71 @@
 <template>
-    <a-layout id="components-layout-demo-top" class="layout">
-        <a-layout-header>
-            <!-- <div class="logo" /> -->
-            <!-- <img
-                src="/images/dolores.png"
-                alt="logo"
-                style="height: 25px width: 25px"
-            > -->
-            <!-- :default-selected-keys="['1']" -->
-            <a-menu
-                theme="dark"
-                mode="horizontal"
-                :style="{ lineHeight: '64px' }"
-            >
-                <a-menu-item key="1">
-                    <a href="/user/dashboard">Dashboard </a></a-menu-item
-                >
-                <a-menu-item key="2">
-                    <a href="/user/application">Application </a></a-menu-item
-                >
-                <a-menu-item key="4">
-                    <span @click="logOut">Logout </span></a-menu-item
-                >
-            </a-menu>
-        </a-layout-header>
-        <a-layout-content style="padding: 0 50px">
-            <!-- <a-breadcrumb style="margin: 16px 0">
-                <a-breadcrumb-item>Home</a-breadcrumb-item>
-                <a-breadcrumb-item>List</a-breadcrumb-item>
-                <a-breadcrumb-item>App</a-breadcrumb-item>
-            </a-breadcrumb> -->
-            <slot></slot>
-        </a-layout-content>
+    <a-layout id="components-layout-demo-custom-trigger">
+        <a-layout-sider
+            v-model="collapsed"
+            :trigger="null"
+            collapsible
+            style="min-height: 100vh"
+        >
+            <div style="width: 100%">
+                <img
+                    src="/images/dolores.png"
+                    alt="logo"
+                    style="
+                        height: 100px;
+                        margin: 10px auto !important;
+                        display: block;
+                    "
+                />
+            </div>
 
-        <a-layout-footer style="text-align: center">
-            Ant Design ©2018 Created by Ant UED
-        </a-layout-footer>
+            <a-menu theme="dark" mode="inline" :default-selected-keys="['0']">
+                <a-menu-item @click="viewPage(`/user/dashboard`)" key="1">
+                    <a-icon type="dashboard" />
+                    <span>Dashboard</span>
+                </a-menu-item>
+                <a-menu-item @click="viewPage(`/user/application`)" key="2">
+                    <a-icon type="form" />
+                    <span>New Application</span>
+                </a-menu-item>
+                <a-menu-item @click="logOut" key="3">
+                    <a-icon type="logout" />
+                    <span>Logout</span>
+                </a-menu-item>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0">
+                <a-icon
+                    class="trigger"
+                    style="padding-top: 20px"
+                    :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                    @click="() => (collapsed = !collapsed)"
+                />
+            </a-layout-header>
+            <a-layout-content
+                :style="{
+                    margin: '24px 16px',
+                    padding: '24px',
+                    background: '#fff',
+                }"
+            >
+                <slot></slot>
+            </a-layout-content>
+        </a-layout>
     </a-layout>
 </template>
 <script>
 export default {
     data() {
         return {
-            //
+            collapsed: false,
         };
     },
     methods: {
+        viewPage(url) {
+            if (url) window.location.href = url;
+        },
         logOut() {
-            let self = this;
             axios({
                 method: "POST",
                 url: "user/logout",
@@ -60,3 +78,22 @@ export default {
     },
 };
 </script>
+<style>
+#components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+}
+</style>
