4e64d29c710663587a10ed36e0cf0b873322b4df
devitcoy <mretcoyy@gmail.com>
2023-11-07 20:09:48 +0800

    Linked User

--- app/Services/Services/UserService.php
+++ app/Services/Services/UserService.php
@@ -1,10 +1,36 @@
 <?php
 
 namespace App\Services\Services;
-
-
+use Illuminate\Support\Facades\Auth;
+use App\Entities\User;
+use App\Entities\Business;
 
 class UserService implements UserServiceInterface
 {
 
+    public function changeRole($data, $id) 
+    {
+
+        $updateData = [
+            'role' => $data['role']
+        ];
+
+        $user = User::find($id);
+        $user->update($updateData);
+
+        return $user;
+    }
+
+    public function linkBusiness($businessID, $userID) 
+    {
+
+        $updateData = [
+            'user_id' => $userID
+        ];
+
+        $business = Business::find($businessID);
+        $business->update($updateData);
+
+        return $business;
+    }
 }
