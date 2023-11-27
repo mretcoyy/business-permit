3eac16d735d93b3e37548c66490e084e4edc9747
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2023-11-27 19:43:35 +0800

    fix: ngrok error

--- resources/views/main/core.blade.php
+++ resources/views/main/core.blade.php
@@ -3,6 +3,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- http fix -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
