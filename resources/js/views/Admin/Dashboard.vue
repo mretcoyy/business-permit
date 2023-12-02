bb8043c254512cbbd9b7e3ba8a5f14f1d1e81050
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2023-12-02 12:57:45 +0800

    feat: update dashboard

--- resources/js/views/Admin/Dashboard.vue
+++ resources/js/views/Admin/Dashboard.vue
@@ -1,120 +1,119 @@
 <template>
     <MainLayout>
-        <a-card>
-            <h1>Dashboard</h1>
-            <div style="padding: 30px">
-                <a-row :gutter="16" justify="space-between">
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 1 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="money-collect" />
-                                Total Income Generated
-                            </template>
-                            <h1 class="text-data">
-                                {{ data.total_income_generated }}
-                            </h1>
-                        </a-card>
-                    </a-col>
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 2 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="money-collect" />
-                                Total Funds
-                            </template>
-                            <h1 class="text-data">{{ data.total_funds }}</h1>
-                        </a-card>
-                    </a-col>
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 2 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="user" />
-                                Total No. of Applicants
-                            </template>
-                            <h1 class="text-data">
-                                {{ data.total_number_applicants }}
-                            </h1>
-                        </a-card>
-                    </a-col>
-                </a-row>
-                <a-row
-                    :gutter="16"
-                    style="margin-top: 10px"
-                    justify="space-between"
+        <div id="dashboard-card" style="padding: 30px">
+            <a-row :gutter="16" justify="space-between">
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 1 }"
                 >
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 1 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="folder-open" />
-                                No. of Pending Applicants
-                            </template>
-                            <h1 class="text-data">
-                                {{ data.number_pending_applicants }}
-                            </h1>
-                        </a-card>
-                    </a-col>
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 2 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="folder-open" />
-                                No. of Renewals
-                            </template>
-                            <h1 class="text-data">
-                                {{ data.number_renewals }}
-                            </h1>
-                        </a-card>
-                    </a-col>
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 2 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="file" />
-                                No. of Ongoing Applications
-                            </template>
-                            <h1 class="text-data">
-                                {{ data.number_ongoing_applications }}
-                            </h1>
-                        </a-card>
-                    </a-col>
-                </a-row>
-                <a-row
-                    :gutter="16"
-                    style="margin-top: 10px"
-                    justify="space-between"
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="money-collect" />
+                            Total Income Generated
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.total_income_generated ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 2 }"
                 >
-                    <a-col
-                        :xs="{ span: 5, offset: 1 }"
-                        :lg="{ span: 6, offset: 1 }"
-                    >
-                        <a-card :bordered="true">
-                            <template #title>
-                                <a-icon type="check" />
-                                No. of Approved Applications
-                            </template>
-                            <h1 class="text-data">
-                                {{ data.number_approved_applications }}
-                            </h1>
-                        </a-card>
-                    </a-col>
-                </a-row>
-            </div>
-        </a-card>
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="money-collect" />
+                            Total Funds
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.total_funds ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 2 }"
+                >
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="user" />
+                            Total No. of Applicants
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.total_number_applicants ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+            </a-row>
+            <a-row
+                :gutter="16"
+                style="margin-top: 10px"
+                justify="space-between"
+            >
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 1 }"
+                >
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="folder-open" />
+                            No. of Pending Applicants
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.number_pending_applicants ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 2 }"
+                >
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="folder-open" />
+                            No. of Renewals
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.number_renewals ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 2 }"
+                >
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="file" />
+                            No. of Ongoing Applications
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.number_ongoing_applications ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+            </a-row>
+            <a-row
+                :gutter="16"
+                style="margin-top: 10px"
+                justify="space-between"
+            >
+                <a-col
+                    :xs="{ span: 5, offset: 1 }"
+                    :lg="{ span: 6, offset: 1 }"
+                >
+                    <a-card :bordered="true">
+                        <template #title>
+                            <a-icon type="check" />
+                            No. of Approved Applications
+                        </template>
+                        <h1 class="text-data">
+                            {{ data.number_approved_applications ?? "-" }}
+                        </h1>
+                    </a-card>
+                </a-col>
+            </a-row>
+        </div>
     </MainLayout>
 </template>
 <script>
@@ -145,5 +144,10 @@ export default {
 <style>
 .text-data {
     font-size: 1.5rem;
+}
+
+#dashboard-card .ant-card-head {
+    background-color: rgba(0, 21, 41, 0.85) !important;
+    color: white !important;
 }
 </style>
