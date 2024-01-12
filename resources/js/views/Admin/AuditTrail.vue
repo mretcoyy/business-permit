c6ba87399a4af2358e6e34a17a0b9eb309f823f8
jovelle19 <jovelle.espinili@gmail.com>
2024-01-12 22:04:29 +0800

    Payment to Audit Trail

--- resources/js/views/Admin/AuditTrail.vue
+++ resources/js/views/Admin/AuditTrail.vue
@@ -1,7 +1,7 @@
 <template>
     <MainLayout>
         <a-card>
-            <h1>Payment</h1>
+            <h1>Audit Trail</h1>
             <a-row :gutter="16">
                 <a-col :span="8">
                     <a-range-picker v-model="date_range" />
